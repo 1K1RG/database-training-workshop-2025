a937250e10122d8d2a7e3abef86a81ec2c459390
garthendrich <garthendrich@gmail.com>
2025-10-08 18:13:32 +0800

    Fix marked renderer
    Broke after updating dependency

--- components/blog/Markdown.js
+++ components/blog/Markdown.js
@@ -3,33 +3,27 @@ import { marked } from 'marked';
 import Prism from 'prismjs';
 
 const renderer = {
-    blockquote(quote) {
-        let match = quote.match?.(/(?<quote>[\w\W]+)?(?:<p>-- (?<caption>[^-]+)(?:- (?<cite>.*))?<\/p>)/m);
+    blockquote(tokens) {
+        let match = tokens.text.match?.(/(?<quote>[\w\W]+)?-- (?<caption>[^-]+)(?:- (?<cite>.*))?$/m);
         if (match && match.groups) {
-            return `<figure><blockquote>${match.groups.quote}</blockquote><figcaption>- ${match.groups.caption} ${ match.groups.cite ? `<cite>${match.groups.cite}</cite>` : '' }</figcaption></figure>`;
+            const quoteText = match.groups.quote.trim();
+            const caption = match.groups.caption.trim();
+            const cite = match.groups.cite?.trim();
+            
+            return `<figure><blockquote><p>${quoteText}</p></blockquote><figcaption>- ${caption}${cite ? ` <cite>${cite}</cite>` : ''}</figcaption></figure>`;
         }
         return false; // return default if no caption
     },
-    code(code, infostring, escaped) {
-        if (infostring) {
-            return `<pre><code class="language-${infostring}">${Prism.highlight(code, Prism.languages[infostring], infostring)}</code></pre>`;
+    code(tokens) {
+        if (tokens.lang) {
+            return `<pre><code class="language-${tokens.lang}">${Prism.highlight(tokens.text, Prism.languages[tokens.lang], tokens.lang)}</code></pre>`;
         }
         return false; // or return default
     },
-    link(href, title, text) {
+    link(tokens) {
         // add target blank to external links
-        if (href.match?.(/http/gi)) {
-            return `<a href="${href}" target="_blank" rel="noopener">${text}</a>`;
-        }
-        return false; // or return default
-    },
-    list(body, ordered, start) {
-        // return letter lists
-        if (!ordered && body.match?.(/^<li>[a-z]\)<\/li>/)) {
-            let match = body.match(/^<li>(?<letter>[a-z])\)<\/li>(?<list>[\w\W]+)?/m); // check if it's a letter list
-            if (match && match.groups && match.groups.letter) {
-                return `<ol type="a" start="${match.groups.letter.charCodeAt(0) - 96}">${match.groups.list}</ol>`;
-            }
+        if (tokens.href.match?.(/http/gi)) {
+            return `<a href="${tokens.href}" target="_blank" rel="noopener">${tokens.text}</a>`;
         }
         return false; // or return default
     },
